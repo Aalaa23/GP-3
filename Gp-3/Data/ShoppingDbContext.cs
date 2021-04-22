309f140507ce8ad03276f7403e01325fed2aa39d
Voli <mohamed.ahmed8902@gmail.com>
2021-04-22 16:28:20 +0200

    Setting the DbContext

--- Gp-3/Data/ShoppingDbContext.cs
+++ Gp-3/Data/ShoppingDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Gp_3.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -13,5 +14,33 @@ namespace Gp_3.Data
         {
             
         }
+        //Users Tables: 
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+
+        //Product Related
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        //Cart Related : 
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartProducts> CartProducts { get; set; }
+
+        //Order Related : 
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<OrderProducts> OrderProducts { get; set; }
+
+        //WishList Related : 
+
+        public DbSet<WishList> WishLists { get; set; }
+        public DbSet<WishListProducts> WishListProducts { get; set; }
+
+        //Addresses Related : 
+        public DbSet<CustomerAddress> Addresses { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
     }
 }
